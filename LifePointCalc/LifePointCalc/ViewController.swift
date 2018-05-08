c6aa7dc1d0a03f6e1f85b3e7f98bb06e769d6293
Nick Wood <mm02minimac@MM02s-Mac-mini.iinet.net.au>
2018-05-08 21:19:59 +1000

    menu
    added simple menu

--- LifePointCalc/LifePointCalc/ViewController.swift
+++ LifePointCalc/LifePointCalc/ViewController.swift
@@ -12,9 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+   
+    @IBOutlet weak var setlifepoint1: UIButton!
+    @IBOutlet weak var setLifePoint2: UIButton!
+    @IBOutlet weak var setLifePoint3: UIButton!
+    
+    @IBAction func PlayersTwo(_ sender: Any) {
+        setlifepoint1.isHidden = false
+        setLifePoint2.isHidden = false
+        setLifePoint3.isHidden = false
+    }
+    @IBAction func PlayersThree(_ sender: Any) {
+        setlifepoint1.isHidden = false
+        setLifePoint2.isHidden = false
+        setLifePoint3.isHidden = false
+    }
+    @IBAction func PlayerFour(_ sender: Any) {
+        setlifepoint1.isHidden = false
+        setLifePoint2.isHidden = false
+        setLifePoint3.isHidden = false
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
