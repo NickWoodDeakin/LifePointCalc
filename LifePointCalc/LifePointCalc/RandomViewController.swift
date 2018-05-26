21ba80ee095d1aef9129afe09c20317fed775485
Nick Wood <mm02minimac@MM02s-Mac-mini.iinet.net.au>
2018-05-26 16:42:22 +1000

    merge of both our work
    mergerd our work

--- LifePointCalc/LifePointCalc/RandomViewController.swift
+++ LifePointCalc/LifePointCalc/RandomViewController.swift
@@ -2,17 +2,17 @@
 //  RandomViewController.swift
 //  LifePointCalc
 //
-//  Created by Nicholas Pilkington on 22/5/18.
+//  Created by Nicholas Pilkington and Nicholas Wood on 22/5/18.
 //  Copyright © 2018 Deakin. All rights reserved.
 //
 
 import UIKit
 
 class RandomViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         if ViewController.diceOrCoin == 0{
             TestLabel.text = "DICE"
@@ -22,7 +22,7 @@ class RandomViewController: UIViewController {
             TestLabel.text = "COIN"
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -35,14 +35,15 @@ class RandomViewController: UIViewController {
     @IBAction func backButtonPress(_ sender: Any) {
         performSegue(withIdentifier: "backFromRandom", sender: nil)
     }
+   
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
